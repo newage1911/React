0b4954fa6b712f523c5477c531f2fd7d5248e8da
newage1911 <newaddsteam2555@gmail.com>
2022-06-26 15:54:33 +0700

    react app weather

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
 
+
 function App() {
+  const name ="Bangkok"
+  const apiKey ="bc341a12ead580937571af41e1d857c6"
+  const [city,setCity] = useState({})
+  const [Loading,setLoading] = useState(false)
+
+  useEffect(()=>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+        setCity(data)
+        setLoading(true)
+    })
+  },[])
+
+  const convertTemp=(k)=>{
+    return (k-273).toFixed()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      (Loading && <div className="App">
+        <section>
+            <div className="location">
+                <h1 className="city">{city.name}</h1>
+                <p className="state">{city.sys.country}</p>
+            </div>
+
+                <div className="card">
+                    <div className="weather">
+                        <h1>{convertTemp(city.main.temp)}&deg;C</h1>
+                        <small>สูงสุด: {convertTemp(city.main.temp_max)}&deg;C , ต่ำสุด: {convertTemp(city.main.temp_min)}&deg;C</small>
+                    </div>
+                    <div className="info">
+                        <div className="status">{city.weather[0].main}</div>
+                        <div className="humidity">ความชื้น: {city.main.humidity}</div>
+                        <div className="wind">ความเร็วลม: {city.wind.speed}</div>
+                    </div>
+                </div>
+            
+        </section>
+    </div>)
   );
 }
 
